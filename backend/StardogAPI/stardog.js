df68c60361ff779fd5c0655858d9a209d76ee9db
Yogev Rahamim <yogev26r@gmail.com>
2020-10-05 17:54:49 +0300

    n

---
+++ backend/StardogAPI/stardog.js
@@ -0,0 +1,29 @@
+const { Connection, query } = require('stardog');
+
+
+const conn = new Connection({
+    username: 'admin',
+    password: 'admin',
+    endpoint: 'http://localhost:5820',
+});
+
+
+const db = 'ef'
+const queryOption = 'application/sparql-results+json'
+const queryParams = { reasoning: true }
+const queryString = 'select distinct ?s ?p ?o where { ?s ?p ?o }'
+
+
+function exequery(callback) {
+    query.execute(conn, db, queryString, queryOption, queryParams).then((body) => callback(body));
+
+
+
+}
+
+
+
+
+
+
+module.exports = { exequery }
