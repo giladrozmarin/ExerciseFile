af98f275c21a18aed346e4f45ebbef11930b5422
Yogev Rahamim <yogev26r@gmail.com>
2020-10-08 15:46:14 +0300

    n

--- backend/RuleEngine/engine.js
+++ backend/RuleEngine/engine.js
@@ -1,7 +1,7 @@
 const Engine = require('../node_modules/json-rules-engine').Engine
 const Rule = require('../node_modules/json-rules-engine').Rule
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 class RuleEngine {
 
@@ -9,26 +9,20 @@ class RuleEngine {
         if (RuleEngine.instance instanceof RuleEngine)
             return RuleEngine.instance
 
-        let rules = this.readRules()
-        this.ruleCount = rules.length + 1;
-        console.log(rules);
-        this.engine = new Engine(rules);
-
+        this.updateRules()
         RuleEngine.instance = this
     }
 
-    readRules() {
-        var jsonPath = path.join(__dirname, 'json-rules.json');
-        let jsonObj = JSON.parse(fs.readFileSync(jsonPath));
-        let jsonArr = Object.entries(jsonObj);
-        jsonArr.map((cell, i) => jsonArr[i] = cell[1]);
-        return jsonArr;
+    updateRules(rules) {
+        console.log(rules)
+        this.engine = new Engine(rules)
     }
 
     addRule(conditions, evt) {
-        let options;
-        conditions.foreach(condition => {
-            options.conditions.any.all.push(
+        let options = { conditions: { any: [{ all: [] }] } };
+
+        conditions.forEach(condition => {
+            options.conditions.any[0].all.push(
                 {
                     fact: condition.fact,
                     operator: condition.operator,
@@ -36,51 +30,16 @@ class RuleEngine {
                 }
             )
         })
-
         options.event = {  // define the event to fire when the conditions evaluate truthy
             type: evt.type,
             params: {
                 message: evt.msg
             }
-
-            // let options = {
-            //     conditions: {
-            //         any: [{
-            //             all: [{
-            //                 fact: cond1.fact,
-            //                 operator: cond1.operator,
-            //                 value: cond1.value
-            //             },
-            //             {
-            //                 fact: cond2.fact,
-            //                 operator:  dic[pred].operator,
-            //                 value:  dic[pred].value
-            //             }]
-            //         }]
-            //     },
-            //     event: {  // define the event to fire when the conditions evaluate truthy
-            //         type: 'Urban-warfarecantuseM113',
-            //         params: {
-            //             message: 'Urban-warfare cant use M113'
-            //         }
-            //     }
         };
 
-
-
         let rule = new Rule(options)
-
         this.engine.addRule(rule)
-
-        newRule = { [this.ruleCount++]: rule }
-        var jsonPath = path.join(__dirname, 'json-rules.json');
-        let jsonString = newRule.toJSON()
-
-        fs.writeFileSync(jsonPath, jsonString, { flag: "wx" }, function (err) {
-            if (err) {
-                console.log(err)
-            }
-        })
+        return rule.toJSON(false);
     }
 
     // Run the engine to evaluate
@@ -94,13 +53,6 @@ class RuleEngine {
                 callback(errors);
             }).catch(reason => console.log(reason))
     }
-
-    // let jsonString = rule.toJSON()
-    //fs.writeFile("json-rules.json", jsonString, function (err) {
-    //  if (err) {
-    //     console.log(err);
-    //     }
-    // });
 }
 
 module.exports = { RuleEngine }
--- backend/StardogAPI/ruleDeriving.js
+++ backend/StardogAPI/ruleDeriving.js
@@ -2,18 +2,19 @@ const { rdfVoc, linksVoc, operatorDic } = require('./vocabulary')
 const { exequery } = require('./stardog');
 const observer = require('./fileObserver');
 const path = require('path');
+const fs = require('fs');
 const RuleEngine = require('../RuleEngine/engine').RuleEngine
 
 const Engine = new RuleEngine();
 
-let filePath = path.join(__dirname, 'ExerciseFileDB.ttl');
-observer.observeFile(filePath, onFileChange)
-filePath = path.join(__dirname, 'ExerciseFileScheme.ttl');
-observer.observeFile(filePath, onFileChange)
-
 let onFileChange = () => {
     getLinks()
 }
+
+let filePath = path.join(__dirname, 'ExerciseFileDB.ttl');
+observer.observeFile(filePath, onFileChange)
+filePath = path.join(__dirname, 'ExerciseFileSchema.ttl');
+observer.observeFile(filePath, onFileChange)
 
 const links = []
 const types = []
@@ -40,17 +41,20 @@ function getLinks() {
             })
             filtered.map((spo) => {
                 linksVoc.find((word) => word === spo.p) ? links.push(spo) :
-                    spo.p == 'type' ? types.push(spo) : null
+                    (spo.p == 'type' && spo.o !== 'owl#Thing') ? types.push(spo) : null
             })
-            console.log(filtered);
-            console.log(count, types, links);
+            // console.log(filtered);
+            // console.log(count, types, links);
 
-            let conditions = []
+            let rules = [];
+            let conditions = [];
             links.forEach(link => {
                 let mainCondition = {};
                 types.forEach(type => {
 
                     if (type.s === link.s) {
+                        // console.log(type.s, link.s);
+
                         mainCondition.fact = type.o;
                         mainCondition.operator = 'equal'
                         mainCondition.value = link.s;
@@ -58,19 +62,38 @@ function getLinks() {
                 })
                 types.forEach(type => {
                     let condition = {};
+                    let evt = {};
                     if (type.o === link.o) {
                         condition.fact = type.s;
                         condition.operator = operatorDic[link.p].operator
                         condition.value = operatorDic[link.p].value
                         conditions.push(mainCondition)
                         conditions.push(condition)
-                        evt.message = link.s + '' + link.p + '' + type.o;
-                        evt.message = link.s + ' ' + operatorDic[link.p].message + ' ' + type.o;
-                        Engine.addRule(conditions, evt)
+                        evt.type = link.s + '' + link.p + '' + type.o;
+                        evt.msg = link.s + ' ' + operatorDic[link.p].message + ' ' + type.o;
+                        // console.log(conditions);
+                        rule = Engine.addRule(conditions, evt)
+                        rules.push(rule);
+                        conditions = []
                     }
                 })
             })
 
+            var jsonPath = path.join(__dirname, 'json-rules.json');
+            let jsonString = JSON.stringify(rules)
+            console.log(JSON.parse(jsonString))
+
+            fs.writeFileSync(jsonPath, jsonString, function (err) {
+                if (err) {
+                    console.log(err)
+                }
+            })
+            let jsonObj = JSON.parse(fs.readFileSync(jsonPath));
+            let jsonArr = Object.entries(jsonObj);
+            jsonArr.map((cell, i) => jsonArr[i] = cell[1]);
+
+            console.log(jsonArr)
+            Engine.updateRules(jsonArr, jsonArr.length)
 
         }
     )
@@ -78,11 +101,6 @@ function getLinks() {
 function beautifyValue(str, char) {
     return str.substring(str.lastIndexOf(char) + 1);
 }
-
-
-
-
-
 
 getLinks()
 
