31c6f176dac3e5a8362202fca3d3a82142120078
Yogev Rahamim <yogev26r@gmail.com>
2020-10-12 16:54:19 +0300

    m

--- backend/InstructEmphasis/ieModel.js
+++ backend/InstructEmphasis/ieModel.js
@@ -4,7 +4,7 @@ const dal = require('./ieDAL');
 function MeansOfExercise(req, res) {
     //{unit: "Artillery battery A", gun: 1, M548: 2, Track: 3, M113: 4,BMP-1: 5, Rocket: 6,Artilley: 7 ,light vehicle: 8}
     let facts = req.body
-    req.body['Terrain'] = 'Urban-warfare';
+    req.body['Exercise-type'] = 'Urban-warfare';
 
     dal.CheckRules(facts, function (engineRes) {
         console.log(engineRes);
@@ -12,23 +12,23 @@ function MeansOfExercise(req, res) {
     });
 }
 
-function MeansOfExerciseData (req, res ){
+function MeansOfExerciseData(req, res) {
 
-    let table1Query ='{$or:[{"o":"APC"},{"o":"Truck"},{"o":"Rocket"},{"o":"Artillery"},{"o":"SPG"}]},{s:1}'
+    let table1Query = '{$or:[{"o":"APC"},{"o":"Truck"},{"o":"Rocket"},{"o":"Artillery"},{"o":"SPG"}]},{s:1}'
 
-   // db.Types.find({$or:[{"o":"APC"},{"o":"Truck"},{"o":"Rocket"},{"o":"Artillery"},{"o":"SPG"}]},{s:1})
+    // db.Types.find({$or:[{"o":"APC"},{"o":"Truck"},{"o":"Rocket"},{"o":"Artillery"},{"o":"SPG"}]},{s:1})
 
     dal.find(table1Query, function (dbRes) {
-         
-       
-      
 
-        dbRes.map((trio, i) => dbRes[i] = {field: trio.s, title:trio.s, type: 'numeric'})
-        
+
+
+
+        dbRes.map((trio, i) => dbRes[i] = { field: trio.s, title: trio.s, type: 'numeric' })
+
         console.table(dbRes);
         res.send({ MeansOfExerciseData: dbRes });
 
     })
-    }
+}
 
-module.exports = { MeansOfExercise,MeansOfExerciseData }
+module.exports = { MeansOfExercise, MeansOfExerciseData }
